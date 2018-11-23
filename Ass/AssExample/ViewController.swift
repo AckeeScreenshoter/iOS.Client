a0b53db6ecf84546a557a9cd8844a57ec9ab5b21
Marek Fořt <marek.fort@ackee.cz>
2018-11-23 22:29:42 +0100

    Change view background color.

--- Ass/AssExample/ViewController.swift
+++ Ass/AssExample/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .red 
     }
 
 
