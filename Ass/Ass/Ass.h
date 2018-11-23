1ca91a5d8eedc67f6d569c6c0e2325c8be388673
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-11-23 21:27:50 +0100

    Initial commit

---
+++ Ass/Ass/Ass.h
@@ -0,0 +1,18 @@
+//
+//  Ass.h
+//  Ass
+//
+//  Created by Jakub Olejník on 23/11/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ass.
+FOUNDATION_EXPORT double AssVersionNumber;
+
+//! Project version string for Ass.
+FOUNDATION_EXPORT const unsigned char AssVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ass/PublicHeader.h>
+
+
---
+++ Ass/AssExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssExample
+//
+//  Created by Jakub Olejník on 23/11/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
