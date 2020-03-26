01d5b1e4775d4bb62a564d9f48ca210f49b3924c
Vendula Svastalova <vendula.svastalova@ackee.cz>
2020-03-27 00:27:47 +0100

    Remove debug info

--- ass-app/ass-app/AppDelegate.swift
+++ ass-app/ass-app/AppDelegate.swift
@@ -11,8 +11,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -33,7 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        print("should open \(url)")
         return true
     }
 }
