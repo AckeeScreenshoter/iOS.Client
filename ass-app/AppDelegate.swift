c3c7253f354e903efa3d2beec9e15337c3112c04
Vendula Švastalová <vendula.svastalova@ackee.cz>
2020-04-08 17:52:56 +0200

    🐛 Fix bug

--- ass-app/AppDelegate.swift
+++ ass-app/AppDelegate.swift
@@ -29,19 +29,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         guard
             var queryItems = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems,
-            let mediaTypeIndex = queryItems.firstIndex(where: { $0.name == Constants.QueryItemKey.mediaType }),
-            let authorizationIndex = queryItems.firstIndex(where: { $0.name == Constants.QueryItemKey.authorization }),
-            let baseURLIndex = queryItems.firstIndex(where: { $0.name == Constants.QueryItemKey.baseURL }),
-            let mediaTypeString = queryItems[mediaTypeIndex].value,
-            let authorization = queryItems[authorizationIndex].value,
-            let baseURL = queryItems[baseURLIndex].value,
+            let mediaTypeString = queryItems.first(where: { $0.name == Constants.QueryItemKey.mediaType })?.value,
+            let authorization = queryItems.first(where: { $0.name == Constants.QueryItemKey.authorization })?.value,
+            let baseURL = queryItems.first(where: { $0.name == Constants.QueryItemKey.baseURL })?.value,
             let mediaType = MediaType(rawValue: mediaTypeString)
             else { return true }
             
-        
-        queryItems.remove(at: mediaTypeIndex)
-        queryItems.remove(at: authorizationIndex)
-        queryItems.remove(at: baseURLIndex)
+        queryItems.removeAll {
+            $0.name == Constants.QueryItemKey.authorization ||
+            $0.name == Constants.QueryItemKey.baseURL ||
+            $0.name == Constants.QueryItemKey.mediaType
+        }
         
         screenShotViewModel?.mediaType = mediaType
         screenShotViewModel?.authorization = authorization
