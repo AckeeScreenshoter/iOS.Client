e4591cae1b617780a99d0807eeb4e7b3b2d4de8d
Vendula Švastalová <vendula.svastalova@ackee.cz>
2020-04-08 16:28:32 +0200

    Remove comment

--- ass-app/Model/Ass.swift
+++ ass-app/Model/Ass.swift
@@ -2,27 +2,10 @@
 //  Ass.swift
 //  Ass
 //
-//  Created by Jakub Olejník on 24/11/2018.
+//  Created by Vendula Švastalová on 26/03/2018.
 //
 
 import UIKit
-
-///// Enum for setting shared configuration
-//public enum Ass {
-//    /// Authorization header for communication with API
-//    public static var authorization: String?
-//
-//    /// BaseURL for sending data
-//    public static var baseURL: URL?
-//
-//    /// Custom data that are appended to default information
-//    public static var customData = CustomData()
-//
-//    internal static var missingFields: [String] {
-//        if baseURL == nil { return ["baseURL"] }
-//        return []
-//    }
-//}
 
 public class Ass: NSObject {
     
