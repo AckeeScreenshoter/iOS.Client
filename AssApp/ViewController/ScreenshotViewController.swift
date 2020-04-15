6ad2b71e1841372e9cded2b031cf20f96d1b6522
Vendula Švastalová <vendula.svastalova@ackee.cz>
2020-04-15 19:06:38 +0200

    Add super.viewDidLoad()

--- AssApp/ViewController/ScreenshotViewController.swift
+++ AssApp/ViewController/ScreenshotViewController.swift
@@ -154,6 +154,7 @@ class ScreenshotViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
