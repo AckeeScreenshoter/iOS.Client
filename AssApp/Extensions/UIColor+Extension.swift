c314f86f065a01a464a4075349d04725744a859c
Vendula Švastalová <vendula.svastalova@ackee.cz>
2020-05-27 16:07:15 +0200

    Create UIColor extension

--- AssApp/Extensions/UIColor+Extension.swift
+++ AssApp/Extensions/UIColor+Extension.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 Vendula Švastalová. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(hex: UInt32) {
+        self.init(
+            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0xFF00) >> 8) / 255.0,
+            blue: CGFloat(hex & 0xFF) / 255.0,
+            alpha: 1.0
+        )
+    }
+    
+    static let ackeePink: UIColor = UIColor(hex: 0xff00ff)
+}
+
