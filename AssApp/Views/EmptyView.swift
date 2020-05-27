8106da01f346f89c2d9fa1a242a544b8f3b5f4b1
Vendula Švastalová <vendula.svastalova@ackee.cz>
2020-05-27 16:19:24 +0200

    💡 Add code documentation

--- AssApp/Views/EmptyView.swift
+++ AssApp/Views/EmptyView.swift
@@ -9,10 +9,13 @@
 import UIKit
 import SnapKit
 
+/// View that only directs the user to the project description
 final class EmptyView: UIView {
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        // checked background
         let background = Asset.background.image.view
         addSubview(background)
         background.snp.makeConstraints { make in
@@ -35,28 +38,26 @@ final class EmptyView: UIView {
             make.centerY.equalToSuperview()
         }
         
-        let gitlabLink = UILabel()
-        gitlabLink.isUserInteractionEnabled = true
-        gitlabLink.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        gitlabLink.textAlignment = .center
+        let gitlabLink = UIButton()
+        gitlabLink.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        gitlabLink.titleLabel?.textAlignment = .center
+        gitlabLink.addTarget(self, action: #selector(openLink), for: .touchUpInside)
         
         let readmeText = """
             Go to GitLab Readme to find out
             more about ASS.
         """
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(openReadme))
-        gitlabLink.addGestureRecognizer(tap)
-        
         let attributedString = NSMutableAttributedString(string: readmeText)
         
+        // Pink coloured text
         let gitlabRange = readmeText.range(of: "GitLab Readme")!
         
         attributedString.setAttributes([
             .foregroundColor: UIColor.ackeePink
         ], range: NSRange(gitlabRange, in: readmeText))
         
-        gitlabLink.attributedText = attributedString
+        gitlabLink.setAttributedTitle(attributedString, for: [])
         addSubview(gitlabLink)
         gitlabLink.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(60)
@@ -64,8 +65,11 @@ final class EmptyView: UIView {
         }
     }
     
+    /// Opens ASS project Readme in GitLab
+    ///
+    /// https://gitlab.ack.ee/iOS/ass/-/blob/master/README.md
     @objc
-    private func openReadme() {
+    private func openLink() {
         let url = URL(string: "https://gitlab.ack.ee/iOS/ass/-/blob/master/README.md")!
         UIApplication.shared.open(url)
     }
