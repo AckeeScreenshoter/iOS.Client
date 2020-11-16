d8255dd32bff9300161ecbe17a972a168fe0c65a
Vendula Švastalová <vendula.svastalova@ackee.cz>
2020-11-16 19:08:52 +0100

    Fix ACKategories dependencies

--- AssApp/Views/LoadingButton.swift
+++ AssApp/Views/LoadingButton.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import ACKategories_iOS
 import SnapKit
 import AssCore
 
--- AssCore/Extensions/UIColor+Extension.swift
+++ AssCore/Extensions/UIColor+Extension.swift
@@ -7,17 +7,9 @@
 //
 
 import UIKit
+import ACKategories_iOS
 
 extension UIColor {
-    public convenience init(hex: UInt32) {
-        self.init(
-            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((hex & 0xFF00) >> 8) / 255.0,
-            blue: CGFloat(hex & 0xFF) / 255.0,
-            alpha: 1.0
-        )
-    }
-    
     public static let ackeePink: UIColor       = UIColor(hex: 0xff00ff)
     public static let ackeePinkDim: UIColor    = UIColor(hex: 0xcc00cc)
     public static let grey: UIColor            = UIColor(hex: 0xeeeeee)
